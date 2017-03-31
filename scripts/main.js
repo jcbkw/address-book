84fa8d64273e2a62aa81dc919024fb2d4ada1df8
jcbkw <diiannasinclar@aol.com>
2017-03-31 18:59:33 -0400

    Build programatic address book document setup

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,35 @@
-function myfunction () {}
+function getContent () {
+
+      var addressData = new XMLHttpRequest;
+      addressData.open("GET", "../contacts.json", true);
+      addressData.send();
+}
+
+function buildAddressBookPage () {
+
+   var mainElement = document.createElement("main"),
+       section1 = document.createElement("section"),
+       section2 = document.createElement("section"),
+       header = document.createElement("header"),
+       ulElement = document.createElement("ul"),
+       liElement = document.createElement("li"),
+       h1Element = document.createElement("h1"),
+       h3Element = document.createElement("h3"),
+       figureElement = document.createElement("figure"),
+       imgElement = document.createElement("img"),
+       addressElement = document.createElement("address"),
+       content = document.createTextNode("text");
+
+    h3Element.appendChild.content();
+
+}
+
+
+
+function initialize () {
+    
+    getContent();
+    buildAddressBookPage();
+
+
+document.addEventListener("load", initialize, false);
