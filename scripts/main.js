097c70ceb3681b86dd16145207baa3b1d4a09336
jcbkw <diiannasinclar@aol.com>
2017-06-14 01:55:10 +0100

    Update code documentation for y

--- scripts/main.js
+++ scripts/main.js
@@ -2,27 +2,18 @@ $(function (){
    
     var contactMap = {};
      
-    function buildContacts (data) {
-        
-       $.get('../templates/contacts.html', function (response){
-           
-           var contacts = Handlebars.compile(response);
-           $(".contact-list").append(contacts(data));
-           console.log(data);
-           console.log(response);
-           
-       }); 
+    function buildContacts (data, template) {
+
+        var contacts = Handlebars.compile(template);
+        $(".contact-list").append(contacts(data));
+       
     }
     
-    function buildAddressBookPage (data) {
+    function buildAddressBookPage (data, template) {
         
-       $.get('../templates/main.html', function (response){
-           
-           var mainPage = Handlebars.compile(response);
-           $("body").append(mainPage(data));
-           
-       }); 
-        
+        var mainPage = Handlebars.compile(template);
+        $("body").append(mainPage(data));
+
     }
 
     function bindEvents(mainElement){
@@ -86,79 +77,139 @@ $(function (){
         xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhr.send(payload);
     }
-
-
-    function loadData(i, objArr, urlArray){
-       //debugger; 
-            
-        $.getJSON(urlArray[i] + ".json", function (content) {
-
-            objArr.push(content);
+    /**
+     * Fetches and parses JSON data using asynchronous requests.
+     * 
+     * When complete, a call back is invoked to pass on this data object and build the rest
+     * of the webpage.
+     *   
+     * @param {number} i The index at which to start reading the 
+     *                   <code>urlArray</code> argument. Usually <code>0</code>.
+     * @param {object} dataObj The object in which the ajax response 
+     *                         data will be added.
+     * @param {string[]} urlArray An array containing the extension-less names
+     *                         of the resources to load.
+     *                         Each element will be prepended with 
+     *                         <code>../data/</code> and appended with
+     *                         <code>.json</code> before the request is sent.
+     * @param {function} callback Function to call back after completion.
+     */
+    function loadData(i, dataObj, urlArray, callback){
+       //Has a recursive helper function which loops through an array of urls
+       //augmenting the data object with reach ajax response.
+     
+         function complete (content) {
+            dataObj[urlArray[i]] = content;
 
             i += 1;
+            
             if (i < urlArray.length) {
                 
-                //console.log(objArr);
-
-                loadData(i, objArr, urlArray);
-
-            }
-        });
-
-    };
-    
-    function loadTemplates(i, templateArr, templateName){
-       debugger; 
-            
-        $.get("../templates/" + templateName[i] + ".html", function (content) {
-
-            templateArr.push(content);
-
-            i += 1;
-            
-            if (i < templateName.length) {
+                $.getJSON("../data/" + urlArray[i] + ".json", complete);
                 
-                //console.log(objArr);
-
-                loadTemplates(i, templateArr, templateName);
-
             }
-        });
+            
+            else {
+                
+                callback();
+                
+            }
+            
+        }
+        $.getJSON("../data/" + urlArray[i] + ".json", complete);
 
     };
     
     /**
-     * 
-     * Gathers template content, and json data content needed to build
-     *  page from two async request.
+     *Manages asynchronous requests for html template data.
+     *When the helper function <code>complete</code> is done, it invokes a 
+     *callback function to pass on the template object
+     *and continue building the webpage.
+     *  
+     * @param {object} templateObj The object in which the ajax response html 
+     *                             data will be added.
+     * @param {string[]} templateNames An array of template names that indicate
+     *                                 each resource to load.
+     *                         
+     * @param {function} callback - Function to call back after completion.
      */
+    function loadTemplates(templateObj, templateNames, callback){
+        
+       var i = 0;
+       //It has a helper function "complete" that recursively calls itself and another helper
+       // "loadTemplate" function until each template in the templateNames array is loaded 
+       //into the template object "templateObj".
+       function complete () {
 
-    function initialize () {
-      var contentArray = [],
-          templateArray = [],
-          dataUrlArray = ["data/content","data/contacts"],
-          templateNameArray = ["contacts", "main"],
-          iterator = 0;
-       loadData(iterator, contentArray, dataUrlArray);
- 
-       loadTemplates(iterator, templateArray, templateNameArray);
-       console.log(contentArray);
-       console.log(templateArray);
-       //loadTemplates();
-//        $.getJSON("data/content.json", function (content) {
-//
-//            $.getJSON("data/contacts.json", function (contacts) {
-//
-//                buildAddressBookPage(content);
-//                buildContacts(contacts);
-//
-//            });
-//
-//       });
+            i += 1;
+
+            if (i < templateNames.length) {
+                //The loadTemplate function fetches each template asynchronously and augments the
+                //templateObj when called.
+                loadTemplate(templateObj, templateNames[i], complete);
+
+            }
+            else{
+
+                callback();
+
+            }
+       }
+        
+       loadTemplate(templateObj, templateNames[i], complete);
 
     }
     
-    // start once ready
+    /**
+     * This function fetches an html template asynchronously and augments the
+     * <code>templateObj</code>when called.
+     * 
+     * @param {object} templateObj The object in which the ajax response html 
+     *                             data will be added.
+     * @param {array} templateName An array of template names that indicate
+     *                             each resource to load.
+     * @param {function} callback  Function to call back after completion.
+     */
+    function loadTemplate(templateObj, templateName, callback){
+        //Second helper function to loadTemplates.   
+        $.get("../templates/" + templateName + ".html", function (content) {
+
+            templateObj[templateName] = content;
+
+            callback(content);
+            
+        });
+    }
+    
+    /**
+     * This function initializes this application.
+     *
+     */
+    function initialize () {
+     
+      var contentObject = {},
+          templateObject = {},
+          dataUrlArray = ["content","contacts"],
+          templateNameArray = ["main", "contacts"],
+          iterator = 0;
+  
+   //Manages asynchronus requests needed to build the main landing page.
+   //Several helper functions gather template content and json content needed to build
+   //the IU and landing pages.
+      loadData(iterator, contentObject, dataUrlArray, function (){
+           
+        loadTemplates(templateObject, templateNameArray, function (){
+
+            buildAddressBookPage(contentObject.content, templateObject.main);
+
+            buildContacts(contentObject.contacts, templateObject.contacts);
+            
+        });
+           
+      });
+       
+    }
+       // start once ready
     
     initialize();
     
